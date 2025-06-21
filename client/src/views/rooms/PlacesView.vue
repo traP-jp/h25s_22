a7fab69ce5d1ce960f866dc4303d29bbb1c172ed
yuiveslkao <titechnatume@gmail.com>
2025-06-21 18:13:27 +0900

    fix CSS

--- client/src/views/rooms/PlacesView.vue
+++ client/src/views/rooms/PlacesView.vue
@@ -45,14 +45,6 @@ const places = ref([
 ])
 
 const selectedPlaceId = ref(2)
-
-// --- ボタンのクリックイベント ---
-const handleVote = () => {
-  const roomId = route.params.room_id
-  const selectedPlace = places.value.find((p) => p.id === selectedPlaceId.value)
-  alert(`Room ID: ${roomId} の「${selectedPlace?.name}」に投票しました！`)
-  // 次のページへ飛ぶ処理
-}
 </script>
 
 <template>
@@ -96,12 +88,12 @@ const handleVote = () => {
           </div>
         </div>
 
-        <div class="mt-8 space-y-4 overflow-y-auto px-12 h-44">
+        <div class="mt-8 space-y-4 overflow-y-auto px-12 flex-12 min-h-0">
           <div
             v-for="place in places"
             :key="place.id"
             @click="selectedPlaceId = place.id"
-            class="flex h-20 w-full cursor-pointer items-center rounded-lg border bg-white p-3 shadow-sm transition-all"
+            class="flex min-h-20 w-full cursor-pointer items-start rounded-lg border bg-white p-3 shadow-sm transition-all"
             :class="{
               'border-green-500 shadow-md ring-1 ring-green-500': selectedPlaceId === place.id,
               'border-gray-200': selectedPlaceId !== place.id,
@@ -116,12 +108,7 @@ const handleVote = () => {
               <p class="font-semibold text-gray-800">{{ place.name }}</p>
               <p class="text-xs text-gray-500">{{ place.address }}</p>
               <div class="mt-1 flex items-center">
-                <svg class="h-4 w-4 text-yellow-400" viewBox="0 0 20 20" fill="currentColor">
-                  <path
-                    d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
-                  />
-                </svg>
-                <span class="ml-1 text-sm font-medium text-gray-700">{{ place.votes }} 票</span>
+                <svg class="h-4 w-4 text-yellow-400" viewBox="0 0 20 20" fill="currentColor"></svg>
               </div>
             </div>
           </div>
