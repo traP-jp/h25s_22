96ebb0aecd92f2b3329aa9536d3ffdd2c7c48da5
otima <otima344@gmail.com>
2025-06-21 19:03:01 +0900

    ルーム作成のハンドラを実装　timeOptionsにroomIDを追加

--- server/internal/handler/room.go
+++ server/internal/handler/room.go
@@ -1,13 +1,129 @@
 package handler
 
 import (
+	"backend/internal/repository"
 	"net/http"
+	"time"
 
+	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
 )
 
+// Room作成リクエスト
+type (
+	TimeOption struct {
+		StartTime time.Time `json:"start_time"` // 開始時間
+		EndTime   time.Time `json:"end_time"`   // 終了時間
+	}
+	CreateRoomRequest struct {
+		TimeOptions   []TimeOption `json:"time_options"`    // 時間候補
+		PlaceOptions  []string     `json:"place_options"`   // 場所候補（GooglePlaceIDの配列）
+		CenterPlaceID string       `json:"center_place_id"` // 中心位置(GooglePlaceID)
+		Radius        float64      `json:"radius"`          // 半径
+		PlaceMax      int          `json:"place_max"`       // 最大値
+	}
+
+	ReturnTimeOption struct {
+		ID        uuid.UUID `json:"id"`         // 時間候補のID
+		StartTime time.Time `json:"start_time"` // 開始時間
+		EndTime   time.Time `json:"end_time"`   // 終了時間
+	}
+	ReturnPlaceOption struct {
+		ID            uuid.UUID `json:"id"`              // 場所候補のID
+		GooglePlaceID uuid.UUID `json:"google_place_id"` // Google Place ID
+	}
+
+	CreateRoomResponse struct {
+		RoomID       string              `json:"room_id"`       // 作成されたルームのID
+		TimeOptions  []ReturnTimeOption  `json:"time_options"`  // 作成された時間候補
+		PlaceOptions []ReturnPlaceOption `json:"place_options"` // 作成された場所候補
+	}
+)
+
 func (h *Handler) CreateRoom(c echo.Context) error {
-	return c.String(http.StatusOK, "pong")
+	var req CreateRoomRequest
+	if err := c.Bind(&req); err != nil { //リクエストをバインド
+		return c.JSON(http.StatusBadRequest, map[string]string{"error": "Invalid request"})
+	}
+	roomID, err := h.repo.CreateRoom(c.Request().Context(), repository.CreateRoomParams{ // ルームを作成
+		PlaceMax:    req.PlaceMax,
+		CenterPoint: req.CenterPlaceID,
+		Radius:      req.Radius,
+	})
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Failed to create room"})
+	}
+	for _, timeOption := range req.TimeOptions { // 時間候補の検証
+		if timeOption.StartTime.IsZero() || timeOption.EndTime.IsZero() {
+			return c.JSON(http.StatusBadRequest, map[string]string{"error": "Invalid time option"})
+		}
+		if timeOption.StartTime.After(timeOption.EndTime) {
+			return c.JSON(http.StatusBadRequest, map[string]string{"error": "Start time must be before end time"})
+		}
+	}
+	timeOptionsWithID := make([]repository.CreateTimeOptionParams, len(req.TimeOptions)) // TimeOptionの作成用スライス
+	placeOptionsWithID := make([]repository.CreatePlaceParams, len(req.PlaceOptions))    // PlaceOptionの作成用スライス
+	for i, timeOption := range req.TimeOptions {
+		timeOptionsWithID[i] = repository.CreateTimeOptionParams{
+			RoomID:    roomID,
+			StartTime: timeOption.StartTime,
+			EndTime:   timeOption.EndTime,
+		}
+	}
+	for i, placeOption := range req.PlaceOptions {
+		googlePlaceID, err := uuid.Parse(placeOption) // GooglePlaceIDのパース
+		if err != nil {
+			return c.JSON(http.StatusBadRequest, map[string]string{"error": "Invalid place option"})
+		}
+		placeOptionsWithID[i] = repository.CreatePlaceParams{
+			RoomID:        roomID,
+			GooglePlaceID: googlePlaceID,
+		}
+	}
+
+	for _, timeOptionWithID := range timeOptionsWithID {
+		if _, err := h.repo.CreateTimeOption(c.Request().Context(), timeOptionWithID); err != nil {
+			return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Failed to create time option"})
+		}
+	}
+	for _, placeOptionWithID := range placeOptionsWithID {
+		if _, err := h.repo.CreatePlace(c.Request().Context(), placeOptionWithID); err != nil {
+			return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Failed to create place option"})
+		}
+	}
+
+	resTimeOptions := make([]ReturnTimeOption, len(timeOptionsWithID))    // レスポンス用のTimeOptionスライス
+	resPlaceOptions := make([]ReturnPlaceOption, len(placeOptionsWithID)) // レスポンス用のPlaceOptionスライス
+
+	roomTimeOptions, err := h.repo.GetTimeOptionsByRoomID(c.Request().Context(), roomID) // ルームIDに紐づくtimeOptionsを取得
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Failed to retrieve time options"})
+	}
+	roomPlaces, err := h.repo.GetPlacesByRoomID(c.Request().Context(), roomID) // ルームIDに紐づくplacesを取得
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Failed to retrieve place options"})
+	}
+
+	for i, timeOption := range roomTimeOptions { // ルームIDに紐づくtimeOptionsをレスポンス用に変換
+		resTimeOptions[i] = ReturnTimeOption{
+			ID:        timeOption.ID,
+			StartTime: timeOption.StartTime,
+			EndTime:   timeOption.EndTime,
+		}
+	}
+	for i, placeOption := range roomPlaces { // ルームIDに紐づくplacesをレスポンス用に変換
+		resPlaceOptions[i] = ReturnPlaceOption{
+			ID:            placeOption.ID,
+			GooglePlaceID: placeOption.GooglePlaceID,
+		}
+	}
+
+	res := CreateRoomResponse{ // レスポンスの構築
+		RoomID:       roomID.String(),
+		TimeOptions:  resTimeOptions,
+		PlaceOptions: resPlaceOptions,
+	}
+	return c.JSON(http.StatusCreated, res)
 }
 func (h *Handler) GetRoom(c echo.Context) error {
 	return c.String(http.StatusOK, "pong")
@@ -21,4 +137,3 @@ func (h *Handler) ChangeVote(c echo.Context) error {
 func (h *Handler) GetResult(c echo.Context) error {
 	return c.String(http.StatusOK, "pong")
 }
-
--- server/internal/repository/timeOption.go
+++ server/internal/repository/timeOption.go
@@ -12,11 +12,13 @@ type (
 	// timeOption table
 	timeOption struct {
 		ID        uuid.UUID `db:"id"`
+		RoomID    uuid.UUID `db:"room_id"`
 		StartTime time.Time `db:"start_time"`
 		EndTime   time.Time `db:"end_time"`
 	}
 
 	CreateTimeOptionParams struct {
+		RoomID    uuid.UUID `db:"room_id"`
 		StartTime time.Time `db:"start_time"`
 		EndTime   time.Time `db:"end_time"`
 	}
@@ -31,9 +33,17 @@ func (r *Repository) GetTimeOptions(ctx context.Context) ([]*timeOption, error) 
 	return timeOptions, nil
 }
 
+func (r *Repository) GetTimeOptionsByRoomID(ctx context.Context, roomID uuid.UUID) ([]*timeOption, error) {
+	timeOptions := []*timeOption{}
+	if err := r.db.SelectContext(ctx, &timeOptions, "SELECT * FROM timeOptions WHERE room_id = ?", roomID); err != nil {
+		return nil, fmt.Errorf("select timeOptions by roomID: %w", err)
+	}
+	return timeOptions, nil
+}
+
 func (r *Repository) CreateTimeOption(ctx context.Context, params CreateTimeOptionParams) (uuid.UUID, error) {
 	timeOptionID := uuid.New()
-	if _, err := r.db.ExecContext(ctx, "INSERT INTO timeOptions (id, start_time, end_time) VALUES (?, ?, ?)", timeOptionID, params.StartTime, params.EndTime); err != nil {
+	if _, err := r.db.ExecContext(ctx, "INSERT INTO timeOptions (id, room_id, start_time, end_time) VALUES (?, ?, ?, ?)", timeOptionID, params.RoomID, params.StartTime, params.EndTime); err != nil {
 		return uuid.Nil, fmt.Errorf("insert timeOption: %w", err)
 	}
 
