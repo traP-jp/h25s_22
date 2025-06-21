87774daa8e390f409e834c3489259ea6888c37c4
sonic <soyakato0830@icloud.com>
2025-06-21 13:15:18 +0900

    CreatedView仮完成

--- client/src/views/rooms/edit/CreatedView.vue
+++ client/src/views/rooms/edit/CreatedView.vue
@@ -1,86 +1,87 @@
 <script setup lang="ts">
 // 作成完了ページのロジックをここに追加
-import { isJSDocOptionalType } from 'typescript';
-import{ref} from 'vue'
+import { isJSDocOptionalType } from 'typescript'
+import { ref } from 'vue'
 
 const room_id = ref('sampleID')
 
-function idcopy(id: string){
+function idcopy(id: string) {
   navigator.clipboard.writeText(id)
 }
 
-function backhome(){
- location.href='https://google.com'
+function backhome() {
+  location.href = 'https://google.com'
 }
 
-function sharelink(){
-
-}
+function sharelink() {}
 </script>
 
 <template>
-  <div class="header">ルームを作成しました！</div>
-  <br>
-  <div class="roomid-box">
-    <div class="box-header">ルームID</div>
-    <p class="idtext">{{ room_id }}
-      <button class="copybutton" @click="idcopy(room_id)">コピーボタン</button>
-    </p>
+  <div id="container">
+    <div class="flex gap-4 flex-wrap">
+    <h1 class="header w-75 size-7">ルームを作成しました！</h1>
+    <div class="roomid-box w-75 size-30 flex gap-4 flex flex-col justify-center items-center">
+      <div class="box-header w-64 size-8">ルームID</div>
+      <div class="size-6 w-64 flex">
+        <div class="idtext flex-auto">{{ room_id }}</div>
+        <button class="copybutton w-6" @click="idcopy(room_id)">コ</button>
+      </div>
+    </div>
+
+    <div class="flex gap-3 flex-wrap">
+      <button class="home-button w-75 size-14 flex-none" @click="backhome">
+        <div class="home-button-text">ホームに戻る</div>
+      </button>
+      <button class="share-button w-75 size-14 flex-none" @click="sharelink">
+        <div class="share-button-text">リンクをシェア</div>
+      </button>
+    </div>
+    </div>
   </div>
-  <button class="home-button" @click="backhome">
-    <div class="home-button-text">
-      ホームに戻る
-    </div>
-  </button>
-  <br>
-  <button class="share-button" @click="sharelink">
-    <div class="share-button-text">
-      リンクをシェア
-    </div>
-  </button>
 </template>
 
 <style>
-.header{
-  font-size: 28px;
+#container {
+  display: grid;
+  grid-template-columns: 50px 300px 50px;
 }
 
-.roomid-box{
+.roomid-box {
   background-color: #d9d9d9;
+  grid-row: 4/5;
+  grid-column: 2/3;
 }
 
-.box-header{
+.box-header {
   text-align: center;
-  font-size: 32px;
 }
 
-.idtext{
+.idtext {
   color: #4a4a4a;
-  font-size: 25px;
 }
 
-.copybutton{
+.copybutton {
   cursor: pointer;
-  text-align:end;
+  text-align: end;
 }
 
-.home-button{
+.home-button {
   background-color: #38a3ee;
   cursor: pointer;
+  border-radius: 10px;
 }
 
-.home-button-text{
-  font-size: 24px;
-  color:#ffffff;
+.home-button-text {
+  color: #ffffff;
 }
 
-.share-button{
-  outline-color: #38a3ee;
-  outline-width: 2px;
+.share-button {
+  border: 2px solid #38a3ee;
+  cursor: pointer;
+  border-radius: 10px;
 }
 
-.share-button-text{
-  font-size: 24px;
-  color:#38a3ee
+.share-button-text {
+  color: #38a3ee;
 }
 </style>
