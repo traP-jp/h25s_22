88ea2f26606ff99d972d16e26badd7b8c1c928a0
mamo <p04snao11@gmail.com>
2025-06-21 12:06:42 +0900

    GetPhoto

--- server/internal/handler/place.go
+++ server/internal/handler/place.go
@@ -1,16 +1,28 @@
 package handler
 
 import (
+	"context"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"googlemaps.github.io/maps"
 )
 
 func (h *Handler) GetSearch(c echo.Context) error {
 	return c.String(http.StatusOK, "pong")
 }
+
+
+
 func (h *Handler) GetPhoto(c echo.Context) error {
-	return c.String(http.StatusOK, "pong")
+	client, err := maps.NewClient(maps.WithAPIKey("api_key"))
+	photoReq := &maps.PlacePhotoRequest{}
+	errors := c.Bind(photoReq)
+	photo, err := client.PlacePhoto(context.Background(), photoReq)
+	if err != nil || errors != nil {
+		return echo.NewHTTPError(http.StatusBadRequest,"fatal err: %s" , err)
+	} 
+	return c.JSON(http.StatusOK,photo)
 }
 func (h *Handler) GetPlace(c echo.Context) error {
 	return c.String(http.StatusOK, "pong")
