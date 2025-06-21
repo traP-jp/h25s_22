73470bd4249ecfbf317671a773066283f454e848
Hueter <kohsukemori@gmail.com>
2025-06-21 14:16:21 +0900

    fix schema

--- server/pkg/database/migrations/1_schema.sql
+++ server/pkg/database/migrations/1_schema.sql
@@ -1,6 +1,6 @@
 -- +goose Up
 CREATE TABLE IF NOT EXISTS `rooms` (
-	`id` uuid NOT NULL,
+	`id` CHAR(36) NOT NULL,
 	`place_max` INT,
 	`center_point` VARCHAR(255) NOT NULL,
 	`radius` FLOAT NOT NULL,
@@ -9,25 +9,25 @@ CREATE TABLE IF NOT EXISTS `rooms` (
 );
 
 CREATE TABLE IF NOT EXISTS `people` (
-	`id` uuid NOT NULL,
-	`room_id` uuid NOT NULL,
+	`id` CHAR(36) NOT NULL,
+	`room_id` CHAR(36) NOT NULL,
 	`user_name` VARCHAR(255),
 	PRIMARY KEY (`id`),
 	FOREIGN KEY (`room_id`) REFERENCES `rooms`(`id`) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS `places` (
-	`id` uuid NOT NULL,
-	`room_id` uuid NOT NULL,
+	`id` CHAR(36) NOT NULL,
+	`room_id` CHAR(36) NOT NULL,
 	`place_id` VARCHAR(255),
 	PRIMARY KEY (`id`),
 	FOREIGN KEY (`room_id`) REFERENCES `rooms`(`id`) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS `votes` (
-	`id` uuid NOT NULL,
-	`people_id` uuid NOT NULL,
-	`place_id` uuid NOT NULL,
+	`id` CHAR(36) NOT NULL,
+	`people_id` CHAR(36) NOT NULL,
+	`place_id` CHAR(36) NOT NULL,
 	`rank` INT NOT NULL,
 	`time` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 	PRIMARY KEY (`id`),
