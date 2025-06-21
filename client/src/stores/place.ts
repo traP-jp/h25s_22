a05794e771aef986f41a9a209969cb91070dfd6d
masky5859 <mathsuky2406@gmail.com>
2025-06-22 00:56:34 +0900

    run format

--- client/src/stores/place.ts
+++ client/src/stores/place.ts
@@ -7,55 +7,55 @@ export const usePlaceStore = defineStore('place', () => {
   const places = ref<Place[]>([])
   const votes = ref<PlaceVote[]>([])
   const selectedPlaces = ref<string[]>([])
-  
+
   // 計算されたプロパティ
   const placeCount = computed(() => places.value.length)
-  
+
   const getPlaceVotes = computed(() => (placeId: string) => {
-    return votes.value.filter(vote => vote.placeId === placeId)
+    return votes.value.filter((vote) => vote.placeId === placeId)
   })
-  
+
   const getPlaceAverageScore = computed(() => (placeId: string) => {
-    const placeVotes = votes.value.filter(vote => vote.placeId === placeId)
+    const placeVotes = votes.value.filter((vote) => vote.placeId === placeId)
     if (placeVotes.length === 0) return 0
     const totalScore = placeVotes.reduce((sum, vote) => sum + vote.score, 0)
     return totalScore / placeVotes.length
   })
-  
+
   // アクション
   const addPlace = (place: Place) => {
     places.value.push(place)
   }
-  
+
   const updatePlace = (placeId: string, updates: Partial<Place>) => {
-    const index = places.value.findIndex(place => place.id === placeId)
+    const index = places.value.findIndex((place) => place.id === placeId)
     if (index !== -1) {
       places.value[index] = { ...places.value[index], ...updates }
     }
   }
-  
+
   const deletePlace = (placeId: string) => {
-    const index = places.value.findIndex(place => place.id === placeId)
+    const index = places.value.findIndex((place) => place.id === placeId)
     if (index !== -1) {
       places.value.splice(index, 1)
     }
     // 関連する投票も削除
-    votes.value = votes.value.filter(vote => vote.placeId !== placeId)
+    votes.value = votes.value.filter((vote) => vote.placeId !== placeId)
   }
-  
+
   const addVote = (vote: PlaceVote) => {
     // 既存の投票があれば更新、なければ追加
     const existingVoteIndex = votes.value.findIndex(
-      v => v.placeId === vote.placeId && v.userId === vote.userId
+      (v) => v.placeId === vote.placeId && v.userId === vote.userId,
     )
-    
+
     if (existingVoteIndex !== -1) {
       votes.value[existingVoteIndex] = vote
     } else {
       votes.value.push(vote)
     }
   }
-  
+
   const togglePlaceSelection = (placeId: string) => {
     const index = selectedPlaces.value.indexOf(placeId)
     if (index !== -1) {
@@ -64,11 +64,11 @@ export const usePlaceStore = defineStore('place', () => {
       selectedPlaces.value.push(placeId)
     }
   }
-  
+
   const clearPlaceSelection = () => {
     selectedPlaces.value = []
   }
-  
+
   return {
     // 状態
     places,
