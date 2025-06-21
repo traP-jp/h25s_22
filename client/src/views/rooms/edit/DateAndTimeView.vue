7ba776625d23787e5f9f44262d28bc9166cf4b1e
yuiveslkao <titechnatume@gmail.com>
2025-06-21 11:48:03 +0900

    fix css style(remove [px])

--- client/src/views/rooms/edit/DateAndTimeView.vue
+++ client/src/views/rooms/edit/DateAndTimeView.vue
@@ -50,7 +50,7 @@ const handleNext = () => {
       class="flex h-[800px] w-[400px] flex-col overflow-hidden border border-gray-200 bg-white shadow-lg"
     >
       <header
-        class="box-border flex h-[38px] w-full shrink-0 items-center justify-end border-b border-gray-200 bg-gray-50 px-5"
+        class="box-border flex h-10 w-full shrink-0 items-center justify-end border-b border-gray-200 bg-gray-50 px-5"
       >
         <svg
           class="h-6 w-6"
@@ -68,11 +68,11 @@ const handleNext = () => {
         </svg>
       </header>
 
-      <div class="box-border w-full flex-grow overflow-y-auto px-[50px] pt-12">
+      <div class="box-border w-full flex-grow overflow-y-auto px-12 pt-12">
         <div class="w-full">
-          <h1 class="mb-4 h-[36px] text-[20px] font-medium leading-[36px]">日付を入力</h1>
+          <h1 class="mb-4 h-9 text-xl font-medium leading-9">日付を入力</h1>
 
-          <div class="box-border mb-3 h-[68px] w-full">
+          <div class="mb-3 w-full">
             <label for="date-input" class="mb-2 block h-5 text-sm font-medium leading-5"
               >日付</label
             >
@@ -85,7 +85,7 @@ const handleNext = () => {
             />
           </div>
 
-          <div class="box-border mb-9 flex h-[69px] gap-3">
+          <div class="mb-9 flex gap-3">
             <div class="w-full">
               <label for="start-time" class="mb-2 block h-5 text-sm font-medium leading-5"
                 >開始時刻</label
@@ -115,13 +115,13 @@ const handleNext = () => {
           <div class="flex h-12 gap-3">
             <button
               @click="handleAdd"
-              class="box-border flex h-12 w-[144px] items-center justify-center rounded-md border border-blue-500 bg-white p-0 text-base font-semibold text-blue-500 transition-all hover:bg-blue-50"
+              class="box-border flex h-12 w-36 items-center justify-center rounded-md border border-blue-500 bg-white p-0 text-base font-semibold text-blue-500 transition-all hover:bg-blue-50"
             >
               追加 ＋
             </button>
             <button
               @click="handleNext"
-              class="box-border flex h-12 w-[144px] items-center justify-center rounded-md border border-transparent bg-blue-500 p-0 text-base font-semibold text-white transition-all hover:bg-blue-600"
+              class="box-border flex h-12 w-36 items-center justify-center rounded-md border border-transparent bg-blue-500 p-0 text-base font-semibold text-white transition-all hover:bg-blue-600"
             >
               次へ →
             </button>
