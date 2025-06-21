1d83cce6e0f13472128b6bf644af0f892b0b0a84
sonic <soyakato0830@icloud.com>
2025-06-21 13:55:40 +0900

    ボタンをBasicButtonに置き換え

--- client/src/views/rooms/edit/CreatedView.vue
+++ client/src/views/rooms/edit/CreatedView.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 // 作成完了ページのロジックをここに追加
+import BasicButton from '@/components/BasicButton.vue'
 import { ref } from 'vue'
 
 const room_id = ref('sampleID')
@@ -17,7 +18,7 @@ function sharelink() {}
 
 <template>
   <div id="container">
-    <div class="flex gap-4 flex-wrap">
+    <div class="flex flex-col gap-4 justify-center items-center">
       <h1 class="w-75 size-7">ルームを作成しました！</h1>
       <div class="roomid-box w-75 size-30 flex gap-4 flex flex-col justify-center items-center">
         <div class="box-header w-64 size-8">ルームID</div>
@@ -26,25 +27,15 @@ function sharelink() {}
           <button class="copybutton w-6" @click="idcopy(room_id)">コ</button>
         </div>
       </div>
-
-      <div class="flex gap-3 flex-wrap">
-        <button class="home-button w-75 size-14 flex-none" @click="backhome">
-          <div class="home-button-text">ホームに戻る</div>
-        </button>
-        <button class="share-button w-75 size-14 flex-none" @click="sharelink">
-          <div class="share-button-text">リンクをシェア</div>
-        </button>
+      <div class="flex gap-3 flex-wrap justify-center items-center">
+        <BasicButton text="ホームに戻る" @click="backhome" />
+        <BasicButton text="リンクをシェア" variant="secondary" @click="sharelink" />
       </div>
     </div>
   </div>
 </template>
 
 <style>
-#container {
-  display: grid;
-  grid-template-columns: 50px 300px 50px;
-}
-
 .roomid-box {
   background-color: #d9d9d9;
   grid-row: 4/5;
