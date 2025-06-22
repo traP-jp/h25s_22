98bfb94321a31536123f8bebb0ea9f0f5eb1a1e3
Tmura <keigo_pooh@i.softbank.jp>
2025-06-22 09:12:17 +0900

    ChooseTimeの実装

--- client/src/views/rooms/ChooseTimeView.vue
+++ client/src/views/rooms/ChooseTimeView.vue
@@ -8,7 +8,7 @@ function goTokouhotipage() {
 }
 
 import BasicButton from '@/components/BasicButton.vue'
-const isActiveList = ref([false,false,false])
+const isActiveList = ref([false, false, false])
 
 const toggle = (index: number) => {
   isActiveList.value[index] = !isActiveList.value[index]
@@ -22,42 +22,28 @@ const toggle = (index: number) => {
     </div>
     <div class="flex-col space-y-2 rounded-none px-4">
       <div v-for="(active, index) in isActiveList" :key="index" class="flex items-center">
-        <button 
-        @click="toggle(index)"
-        :class="active ?'bg-sky-400 text-white' : 'bg-gray-200 text-gray-900'"
-        class="py-4 px-6 min-w-[245px] text-left"
+        <button
+          @click="toggle(index)"
+          :class="active ? 'bg-sky-400 text-white' : 'bg-gray-200 text-gray-900'"
+          class="py-4 px-6 min-w-[245px] text-left"
         >
-        日付<br />時間帯                        
+          日付<br />時間帯
         </button>
 
         <button
-        @click="toggle(index)"
-        :class="active ?'bg-sky-400 text-white' : 'bg-gray-200 text-gray-900'"
-        class="flex items-center justify-center w-13.5 h-20 pr-4"
+          @click="toggle(index)"
+          :class="active ? 'bg-sky-400 text-white' : 'bg-gray-200 text-gray-900'"
+          class="flex items-center justify-center w-13.5 h-20 pr-4"
         >
-    
-        <div
-        class="flex items-center justify-center w-5 h-5 rounded-full bg-white border"
-        >
-      
-        <div
-        v-if="active"
-        class="w-3 h-3 rounded-full bg-sky-400"
-        ></div>
-        </div>
+          <div class="flex items-center justify-center w-5 h-5 rounded-full bg-white border">
+            <div v-if="active" class="w-3 h-3 rounded-full bg-sky-400"></div>
+          </div>
         </button>
-        
       </div>
-
-
-
     </div>
-    
-    <div class="flex rounded-lg p-4 items-center"> 
-       <BasicButton 
-       @click="goTokouhotipage"
-       text="次へ  →" variant="primary" size="large"
-       />
+
+    <div class="flex rounded-lg p-4 items-center">
+      <BasicButton @click="goTokouhotipage" text="次へ  →" variant="primary" size="large" />
     </div>
   </div>
 </template>
