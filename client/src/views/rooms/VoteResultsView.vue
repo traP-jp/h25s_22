bd7657ea46a2231b1e3c31a12687a20d2d0fb578
yuiveslkao <titechnatume@gmail.com>
2025-06-22 09:08:21 +0900

    erase useless code

--- client/src/views/rooms/VoteResultsView.vue
+++ client/src/views/rooms/VoteResultsView.vue
@@ -62,25 +62,6 @@ const isPlaceCardOpen = ref(true)
     <div
       class="flex h-[800px] w-[400px] flex-col overflow-hidden rounded-xl border bg-gray-50 shadow-lg"
     >
-      <header
-        class="flex h-10 w-full shrink-0 items-center justify-end border-b border-gray-200 bg-white px-4"
-      >
-        <svg
-          class="h-6 w-6 text-gray-700"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke-width="1.5"
-          stroke="currentColor"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-          />
-        </svg>
-      </header>
-
       <main class="flex-grow p-6">
         <div class="mb-6">
           <h1 class="text-2xl font-bold text-gray-800">{{ eventDetails.title }}</h1>
@@ -98,7 +79,6 @@ const isPlaceCardOpen = ref(true)
                   clip-rule="evenodd"
                 />
               </svg>
-              <span>{{ eventDetails.date }}</span>
             </div>
             <div class="flex items-center">
               <svg
